60c1eda7c1105d81ecfc8037fa73f07c22f8caa6
Zeeshan Ali <zeeshan.ali.ibex@outlook.com>
2022-05-18 04:16:14 +0500

    new theme

--- app/server.js
+++ app/server.js
@@ -9,6 +9,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 app.use(express.static('public'))
+app.use(express.static('public'))
 const config = {
   host: "localhost",
   app: "app",
@@ -34,11 +35,18 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
+app.set('view engine', 'ejs');
 
-router.get('/', (req, res) => {
+router.get('/app', (req, res) => {
   const url = req.params.id;
-  res.sendFile(`${__dirname}/index.html`);
+  res.render(`pages/index`);
 });
+
+router.get('/app/old', (req, res) => {
+  const url = req.params.id;
+  res.sendFile(`${__dirname}/_index.html`);
+});
+
 
 router.get('/api/user-images/:id', (req, res) => {
     const url = req.params.id;
