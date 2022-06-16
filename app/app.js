ffaa2cbde5f8086357fa2c11b3a623e94aaf1ba8
Zeeshan Ali <zeeshan.ali.ibex@outlook.com>
2022-06-16 05:14:57 +0500

    admin

--- app/app.js
+++ app/app.js
@@ -5,6 +5,12 @@ const port = 3000
 const mongoose = require('mongoose');
 const appusers = require("../app/models/appuser.js")
 const bcrypt = require('bcrypt');
+const session = require('express-session');
+const flash = require('connect-flash');
+const passport = require('passport');
+require("../app/config/passport")(passport);
+const {ensureAuthenticated} = require("../app/config/auth.js")
+
 
 //mongoose
 mongoose.connect('mongodb+srv://dtimageeditor:Xcccccc123@cluster0.gte2f0n.mongodb.net/dtimageeditor',{useNewUrlParser: true, useUnifiedTopology : true})
@@ -16,19 +22,54 @@ app.use(express.static('public'))
 app.use(express.urlencoded({extended : false}));
 
 
-router.get('/app', (req, res) => {
-    res.render(`pages/index`);
+app.use(session({
+  secret : 'secret',
+  resave : true,
+  saveUninitialized : true
+ }));
+ app.use(passport.initialize());
+app.use(passport.session());
+
+ //use flash
+ app.use(flash());
+ app.use((req,res,next)=> {
+   res.locals.success_msg = req.flash('success_msg');
+   res.locals.error_msg = req.flash('error_msg');
+   res.locals.error  = req.flash('error');
+ next();
+ })
+
+
+router.get('/app', ensureAuthenticated, (req, res) => {
+    res.render(`pages/client/index`,{
+      user: req.user
+      });
+  });
+
+  router.get('/app/admin',  (req, res) => {
+    res.render(`pages/admin/index`);
   });
 
   router.get('/app/register', (req, res) => {
-    res.render(`pages/register`);   
+    res.render(`pages/client/register`);   
   });
 
   router.get('/app/login', (req, res) => {
-    res.render(`pages/login`);
+    res.render(`pages/client/login`);
   });
   
-  router.post('/app/register', (req, res) => {
+  
+  router.post('/app/client/login', (req, res, next) => {
+    console.log(res);
+    passport.authenticate('local',{
+      successRedirect : '/app/',
+      failureRedirect : '/app/login',
+      failureFlash : true,
+      })(req,res,next);
+  });
+  
+
+  router.post('/app/client/register', (req, res) => {
    
     const {fname,lname,email,password, password2,company_name} = req.body;
     let errors = [];
@@ -48,7 +89,7 @@ router.get('/app', (req, res) => {
     }
     if(errors.length > 0 ) {
     
-        res.render('pages/register', {
+        res.render('pages/client/register', {
         errors : errors,
         name : fname,
         email : email,
@@ -61,7 +102,7 @@ router.get('/app', (req, res) => {
         console.log(user);   
         if(user) {
             errors.push({msg: 'email already registered'});
-            render('pages/register', res,errors,fname,email,password,password2);
+            render('pages/client/register', res,errors,fname,email,password,password2);
             
            } else {
             const newUser = new appusers({
@@ -83,7 +124,9 @@ router.get('/app', (req, res) => {
                                 newUser.save()
                                 .then((value)=>{
                                     console.log(value)
-                                res.redirect('/app/login');
+                                    req.flash('success_msg','You have now registered!')
+
+                                res.redirect('/app/client/login');
                                 })
                                 .catch(value=> console.log(value));
                                   
