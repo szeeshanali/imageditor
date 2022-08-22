e1fa42afa7807e7e56b9c43fb188c1e87889820b
Zeeshan Ali <zeeshan.ali.ibex@outlook.com>
2022-08-22 22:28:28 +0500

    fixed

---
+++ app/models/contents.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const schema  = new mongoose.Schema({
+    content        :    {   type    :   String },
+    type           :    {   type    :   String },
+    deleted        :    {   type    :   Boolean, default : false },
+    active         :    {   type    :   Boolean, default : true },
+    created_dt     :    {   type    :   Date,   default : Date.now },
+    modified_dt    :    {   type    :   Date,   default : null },
+    by_admin       :    {   type    :   Boolean },
+    });
+
+const contents   = mongoose.model('contents',schema);
+module.exports  = contents;
