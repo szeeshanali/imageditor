7f1c68f43d385516c5b298d32e3743add5cbb7c5
Zeeshan Ali <zeeshan.ali.ibex@outlook.com>
2023-03-04 02:11:19 +0500

    fixes

--- app/services/common.js
+++ app/services/common.js
@@ -81,7 +81,7 @@ const commonService = (function() {
     this.getUploads = (type,all) =>
     { 
         let filter = {}; 
-        let projection = {code:1,title:1,type:1,name:1,ref_code:1,id:1,category:1,file_ext:1,active:1 };
+        let projection = {code:1,title:1,type:1,name:1,ref_code:1,id:1,category:1,file_ext:1,active:1,base64:1 };
 
         if(all)
         { 
@@ -91,7 +91,7 @@ const commonService = (function() {
         }
         else 
         { filter = {type: type, active:true, deleted:false }; }
-
+        
         return uploads.find(filter,projection).sort({order_no:1}); 
              
     }
