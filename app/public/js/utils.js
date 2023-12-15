4ba06da13be7d936bc3d55f099512f0bcd4b0489
Zeeshan Ali7 <zeeshan.ali7@corp.ibexglobal.com>
2023-12-15 14:29:30 +0500

    Fixed Reporting Date

--- app/public/js/utils.js
+++ app/public/js/utils.js
@@ -251,7 +251,7 @@ function getCanvasCenter(objectWidth,objectHeight){
 }
 
 function getFormattedDate(dt){
-    dt = new Date(dt.toUTCString());
+    
     const month = (dt.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-based
     const day = dt.getDate().toString().padStart(2, '0');
     const year = dt.getFullYear();
